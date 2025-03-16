cd7ac7cad8c37b17f65718ab07768cd92eeb227f
swaqq-y <kir765x@gmail.com>
2025-03-16 18:49:21 +0300

    Пофиксил custom_flipcoin()

--- funstuff/randomfuncs.py
+++ funstuff/randomfuncs.py
@@ -4,7 +4,7 @@ def flipcoin():
     return random.choice(["Heads", "Tails"])
 def custom_flipcoin(variant1, variant2):
     if isinstance(variant1, str) and isinstance(variant2, str):
-        return random.choice(variant1, variant2)
+        return random.choice([variant1, variant2])
     else:
         return "All two options must be in str format."
 def rolldice():
