8190e7d149f8ba5e5b60531c5927f1954a81678a
swaqq-y <kir765x@gmail.com>
2025-03-16 19:15:51 +0300

    Добавил rock_paper_scissors()

--- funstuff/randomfuncs.py
+++ funstuff/randomfuncs.py
@@ -9,3 +9,23 @@ def custom_flipcoin(variant1, variant2):
         return "All two options must be in str format."
 def rolldice():
     return random.randint(1, 6)
+def custom_rolldice(variant1, variant2, variant3, variant4, variant5, variant6):
+    return random.choice([variant1, variant2, variant3, variant4, variant5, variant6])
+def rock_paper_scissors(variant):
+    res = ''
+    chos = random.choice(["rock", "paper", "scissors"])
+    if variant.lower() == "rock" and chos == "scissors":
+        res == 'Win!!'
+    elif variant.lower() == "paper" and chos == "rock":
+        res == 'Win!!'
+    elif variant.lower() == "scissors" and chos == "paper":
+        res == "Win!!"
+    elif variant.lower == chos:
+        res == "Drawn game!"
+    elif variant.lower() == "paper" and chos == "scissors":
+        res == 'Lose:('
+    elif variant.lower() == "rock" and chos == "paper":
+        res == 'Lose:('
+    elif variant.lower() == "scissors" and chos == "rock":
+        res == 'Lose:('
+    return res
